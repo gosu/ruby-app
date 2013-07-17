9f57df02cb8d40ba6916ea799c059c7cc63309d0
Julian Raschke <julian@raschke.de>
2013-07-17 15:10:25 +0200

    Initial Commit

---
+++ Ruby/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  Ruby
+//
+//  Created by Julian Raschke on 17.07.13.
+//  Copyright (c) 2013 Julian Raschke. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Ruby/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Ruby
+//
+//  Created by Julian Raschke on 17.07.13.
+//  Copyright (c) 2013 Julian Raschke. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
