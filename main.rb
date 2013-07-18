760b588993843e93c256c4efe69b4f6bcd6c893b
Julian Raschke <julian@raschke.de>
2013-07-18 02:15:04 +0200

    Updated icon & placeholder main.rb

--- main.rb
+++ main.rb
@@ -3,14 +3,14 @@ require 'gosu'
 
 class Window < Gosu::Window
   def initialize
-    super 640, 480, false
+    super 800, 600, false
 
     self.caption = "Your Ruby game/app goes here"
     @image = Gosu::Image.new(self, "Ruby.icns", false)
   end
 
   def draw
-    @image.draw_rot self.width / 2, self.height / 2, 0, Gosu::milliseconds / 10.000
+    @image.draw_rot self.width / 2, self.height / 2, 0, Math.sin(Gosu::milliseconds / 500.0) * 5
   end
 
   def update
